041413116cb48ba9f697e46d88a38e834be1861e
ajosuemendez <ajosuemendez@gmail.com>
2025-05-17 19:27:40 +0200

    feat: :sparkles: add image to card

--- frontend/src/app/components/card/card.component.ts
+++ frontend/src/app/components/card/card.component.ts
@@ -1,5 +1,5 @@
 import { CommonModule } from '@angular/common';
-import { Component, Input } from '@angular/core';
+import { Component, Input, ViewChild, ElementRef, OnInit, OnDestroy, AfterViewInit } from '@angular/core';
 
 @Component({
   selector: 'card',
@@ -8,10 +8,33 @@ import { Component, Input } from '@angular/core';
   templateUrl: './card.component.html',
   styleUrls: ['./card.component.css'],
 })
-export class CardComponent {
+export class CardComponent implements OnInit, OnDestroy {
   @Input() project: any;  // Accepts a single project object
+
+  @ViewChild('cardRef') cardRef!: ElementRef<HTMLDivElement>;
+
+  isMobile = false;
+
+  ngOnInit(): void {
+    this.checkMobile();
+    window.addEventListener('resize', this.checkMobile);
+  }
+
+  ngOnDestroy(): void {
+    window.removeEventListener('resize', this.checkMobile);
+  }
+
+  checkMobile = () => {
+    this.isMobile = window.innerWidth <= 768;
+  }
 
   toggleDetails() {
     this.project.expanded = !this.project.expanded;
+
+    if (this.project.expanded) {
+      setTimeout(() => {
+        this.cardRef.nativeElement.scrollIntoView({ behavior: 'smooth', block: 'start' });
+      }, 300); // Adjust delay to match CSS transition duration
+    }
   }
 }
