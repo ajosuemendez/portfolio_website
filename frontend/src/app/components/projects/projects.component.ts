2c27e30576d5ecd26fabfaae08e952afc9b59ad6
ajosuemendez <ajosuemendez@gmail.com>
2025-05-17 18:43:40 +0200

    fix: :bug: fix flickering blue color on mobile

--- frontend/src/app/components/projects/projects.component.ts
+++ frontend/src/app/components/projects/projects.component.ts
@@ -14,14 +14,32 @@ export class ProjectsComponent {
   projects = [
     {
       title: 'Portfolio Website',
-      tech: ['Angular', 'SCSS', 'TypeScript'],
+      tech: ['Angular', 'TypeScript', 'Nginx', 'Docker'],
       description: 'A personal portfolio showcasing my work and resume.',
       expanded: false
     },
     {
-      title: 'E-commerce Store',
-      tech: ['React', 'Node.js', 'MongoDB'],
-      description: 'An online store with user authentication and shopping cart.',
+      title: 'Online Chatbot Escape Room',
+      tech: ['React', 'Python', 'Rasa', 'Docker'],
+      description: 'An online chatbot for playing escape rooms.',
+      expanded: false
+    },
+    {
+      title: 'Feelinguo',
+      tech: ['Angular', 'TypeScript', 'Node.js'],
+      description: 'A platform for learning languages in a fun and easy way.',
+      expanded: false
+    },
+    {
+      title: 'Decision Support App - Slack',
+      tech: ['React', 'Python', 'MySQL'],
+      description: 'App that helps groups to make important decision while traveling on the phone/tablet.',
+      expanded: false
+    },
+    {
+      title: '3 Player Chess App',
+      tech: ['React', 'Java'],
+      description: 'App for scanning rooms and generate 3d models.',
       expanded: false
     },
   ];
