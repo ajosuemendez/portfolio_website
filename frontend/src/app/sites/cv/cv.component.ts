d5abf62b9a20d453cc8e67969c20c2d840fecdab
ajosuemendez <ajosuemendez@gmail.com>
2025-05-17 22:24:31 +0200

    feat: :sparkles: add cv page

--- frontend/src/app/sites/cv/cv.component.ts
+++ frontend/src/app/sites/cv/cv.component.ts
@@ -1,11 +1,15 @@
-// product-list.component.ts
 import { Component } from '@angular/core';
+import { PdfViewerModule } from 'ng2-pdf-viewer';
+
 
 @Component({
   selector: 'cv',
   standalone: true,
-  imports: [],
+  imports: [PdfViewerModule],
   templateUrl: './cv.component.html',
   styleUrls: ['./cv.component.css'],
 })
-export class CvComponent {}
+export class CvComponent {
+  pdfSrc = "assets/cvs/Lebenslauf_Alejandro_Mendez.pdf";
+
+}
