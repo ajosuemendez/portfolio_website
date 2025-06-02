0fb5a42ebc9432c7f00a3afd5c1a952ce71c8d9b
ajosuemendez <ajosuemendez@gmail.com>
2025-06-02 09:22:56 +0200

    add overlay

--- frontend/src/app/sites/home/home.component.ts
+++ frontend/src/app/sites/home/home.component.ts
@@ -1,6 +1,6 @@
 // product-list.component.ts
 import { CommonModule } from '@angular/common';
-import { Component, OnDestroy, OnInit } from '@angular/core';
+import { AfterViewInit, Component, ElementRef, OnDestroy, OnInit, ViewChild } from '@angular/core';
 import { ProjectsComponent } from '../../components/projects/projects.component';
 import { LanguageService } from '../../services/language.service';
 import { Subscription } from 'rxjs';
@@ -12,9 +12,11 @@ import { Subscription } from 'rxjs';
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.css'],
 })
-export class HomeComponent implements OnInit, OnDestroy {
+export class HomeComponent implements OnInit, OnDestroy, AfterViewInit  {
   selectedLang: 'en' | 'de' = 'en';
   private langSub!: Subscription;
+  @ViewChild('languageSwitchContainer') languageSwitchContainer!: ElementRef<HTMLDivElement>;
+  topOverlayHeight: number = 0;
 
   constructor(private languageService: LanguageService) {}
 
@@ -23,6 +25,14 @@ export class HomeComponent implements OnInit, OnDestroy {
       this.selectedLang = lang;
     });
   }
+
+   ngAfterViewInit(): void {
+  setTimeout(() => {
+    const rect = this.languageSwitchContainer.nativeElement.getBoundingClientRect();
+    this.topOverlayHeight = rect.top + rect.height / 2;
+    console.log('Overlay height:', this.topOverlayHeight);
+  });
+}
 
   ngOnDestroy() {
     if (this.langSub) {
